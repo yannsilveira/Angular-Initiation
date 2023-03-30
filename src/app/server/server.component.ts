c644019f0117d1aa6fe5cd85c75d018c1cfcc859
Yann Ap. da Silveira <56853103+yannsilveira@users.noreply.github.com>
2023-03-30 19:22:30 -0300

    Continuing the studies of Angular.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,52 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "app-server",
-    templateUrl: "./server.component.html"
+    templateUrl: "./server.component.html",
+    styleUrls: ["./server.component.css"]
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit{
+    serverId: number = 10;
+    serverStatus: string = "offline";
+    allowNewServer = false;
+    serverCreationStatus = 'No server was created';
+    serverName = '';
+    userName = '';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    constructor() {
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 3000);
+    }
+
+    ngOnInit(): void {
+        
+    }
+
+    onCreateServer() {
+        this.serverCreationStatus = 'Server was created, its name is ' + this.serverName;
+    }
+
+    onUpdateServerName(event: any) {
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
+
+
+    resetUserName(): void {
+        this.userName = '';
+    }
+
+    onUpdateUsername(event: any) {
+        this.userName = (<HTMLInputElement>event.target).value;
+    }
+
+    checkUserNameNotEmpty(): boolean {
+        return this.userName === '';
+    }
 
 
 }
